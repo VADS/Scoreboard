a4945040c8f826c4b290c654d3fbde8bd27bf7b3
Bastian Hodapp <bastianhodapp@gmail.com>
2021-04-01 18:41:07 +0200

    Press two buttons simultaneously for spacebar on Arduino

--- scoreboard_arduino/scoreboard_arduino.ino
+++ scoreboard_arduino/scoreboard_arduino.ino
@@ -1,21 +1,54 @@
 #include "Keyboard.h"
 
 // set pin numbers for the five buttons:
-const int leftButton = 4;
+const int leftButton = 6;
 const int rightButton = 5;
 
+long gracePeriodTimer = 0;
+long gracePeriodAmount = 1000;
+
+bool leftButtonState, rightButtonState;
+bool lastLeftButtonState, lastRightButtonState;
+bool bothButtonsPressed, noButtonsPressed;
+bool timerStarted = false;
+
 void setup() {
-  pinMode(leftButton, INPUT);
-  pinMode(rightButton, INPUT);
+  pinMode(leftButton, INPUT_PULLUP);
+  pinMode(rightButton, INPUT_PULLUP);
 
   Keyboard.begin();
 }
 
 void loop() {
-  if (digitalRead(leftButton) == HIGH) {
-    Keyboard.write('e');
+  lastLeftButtonState = leftButtonState;
+  lastRightButtonState = rightButtonState;
+
+  leftButtonState = digitalRead(leftButton) == LOW;
+  rightButtonState = digitalRead(rightButton) == LOW;
+
+  bothButtonsPressed = leftButtonState && rightButtonState;
+  noButtonsPressed = !(leftButtonState || rightButtonState);
+
+  if (bothButtonsPressed) {
+    if (!timerStarted) {
+      gracePeriodTimer = millis();
+      timerStarted = true;
+    }
   }
-  if (digitalRead(rightButton) == HIGH) {
-    Keyboard.write('q');
+
+  if (!timerStarted) {
+    if (leftButtonState != lastLeftButtonState && !leftButtonState) {
+      Keyboard.write('e');
+    }
+    if (rightButtonState != lastRightButtonState && !rightButtonState) {
+      Keyboard.write('q');
+    }
+  }
+
+   if (noButtonsPressed) {
+    if (timerStarted && millis() < gracePeriodAmount + gracePeriodTimer ) {
+      Keyboard.write(' ');
+    }
+    timerStarted = false;
   }
 }
